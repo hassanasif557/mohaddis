39ff62ba276684ebbcee3644b52b120273f97dd5
hassanasif557 <hassanallahditta557@gmail.com>
2022-09-06 18:09:42 +0500

    6 sept 2022

--- lib/FrontEnd/BookScreen.dart
+++ lib/FrontEnd/BookScreen.dart
@@ -1,4 +1,10 @@
+import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
+import 'package:mohaddis/FrontEnd/HomeScreen.dart';
+import 'package:mohaddis/NavMenuScreens/AboutScreen.dart';
+import 'package:mohaddis/NavMenuScreens/ContactScreen.dart';
+import 'package:mohaddis/NavMenuScreens/PropertiesScreen.dart';
+import 'package:mohaddis/NavMenuScreens/SupportScreen.dart';
 
 class BookScreen extends StatefulWidget {
   const BookScreen({Key? key}) : super(key: key);
@@ -8,8 +14,458 @@ class BookScreen extends StatefulWidget {
 }
 
 class _BookScreenState extends State<BookScreen> {
+
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      key: _scaffoldKey,
+      drawer: _drawer(),
+      backgroundColor: Colors.white,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(60), // Set this height
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(
+                  'assets/images/rectangle_798.png'),
+              fit: BoxFit.fill,
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.only(top: 30.0),
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 20.0,top: 10.0),
+                  child: Column(
+                    children: [
+                      GestureDetector(
+                        onTap:(){
+                          _scaffoldKey.currentState?.openDrawer();
+                        },
+                        child: Container(
+                          width: 35,
+                          height: 35,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage(
+                                  'assets/images/group_36.png'),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    children: [
+                      Center(
+                          child: Padding(
+                            padding: const EdgeInsets.only(top: 6.0),
+                            child: Text('کتب احادیث',
+                              style: TextStyle(
+                                  fontSize: 14.0,
+                                  color: Colors.white,
+                                  fontFamily: 'NotoNastaliqUrdu'
+                              ),),
+                          )
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 20.0,top: 20.0),
+                  child: Column(
+                    children: [
+                      GestureDetector(
+                        onTap:(){
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (_) => HomeScreen()));
+                        },
+                        child: Container(
+                          width: 25,
+                          height: 15,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage(
+                                  'assets/images/group_38.png'),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          SizedBox(height: 55.0,),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Container(
+              child: Text('کتاب منتخب کریں',
+              style: TextStyle(
+                fontSize: 20.0,
+                fontFamily: 'NotoNastaliqUrdu'
+              ),)
+            ),
+          ),
+          SizedBox(height: 50,),
+          Container(
+            child:  GridView.count(
+              crossAxisCount: 3,
+              padding: EdgeInsets.only(left: 30, right: 30, top: 30,bottom: 10),
+              shrinkWrap: true,
+              children:[
+                GestureDetector(
+                  onTap: (){
+
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Color.fromRGBO(230, 230, 230, 0.5)),
+                      color: Colors.white,
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          width: 35,
+                          height: 25,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage(
+                                  'assets/images/forma_1.png'),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),Text("سنن أبي داؤد", style:TextStyle(color: Colors.black, fontSize: 12.0))
+                      ],
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: (){
+
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Color.fromRGBO(230, 230, 230, 0.5)),
+                      color: Colors.white,
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          width: 35,
+                          height: 25,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage(
+                                  'assets/images/forma_1.png'),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),Text("صحيح مسلم", style:TextStyle(color: Colors.black, fontSize: 12.0))
+                      ],
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: (){
+
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Color.fromRGBO(230, 230, 230, 0.5)),
+                      color: Colors.white,
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          width: 35,
+                          height: 25,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage(
+                                  'assets/images/forma_1.png'),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),Text("صحیح بخاری ", style:TextStyle(color: Colors.black, fontSize: 12.0))
+                      ],
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: (){
+
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Color.fromRGBO(230, 230, 230, 0.5)),
+                      color: Colors.white,
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          width: 35,
+                          height: 25,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage(
+                                  'assets/images/forma_1.png'),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),Text("سنن ابن ماجه", style:TextStyle(color: Colors.black, fontSize: 12.0))
+                      ],
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: (){
+
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Color.fromRGBO(230, 230, 230, 0.5)),
+                      color: Colors.white,
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          width: 35,
+                          height: 25,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage(
+                                  'assets/images/forma_1.png'),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),Text("سنن النسائي", style:TextStyle(color: Colors.black, fontSize: 12.0))
+                      ],
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: (){
+
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Color.fromRGBO(230, 230, 230, 0.5)),
+                      color: Colors.white,
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          width: 35,
+                          height: 25,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage(
+                                  'assets/images/forma_1.png'),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),Text("جامع الترمذي", style:TextStyle(color: Colors.black, fontSize: 12.0))
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+
+            ),
+          ),
+          Expanded(
+            child: Align(
+              alignment: FractionalOffset.bottomCenter,
+              child: Container(
+                width: double.maxFinite,
+                height: 130,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(
+                        'assets/images/layer_1699.png'),
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+
+
+  Widget _drawer(){
+    return Drawer(
+      elevation: 10.0,
+      child: Container(
+        width: double.maxFinite,
+        height: double.maxFinite,
+        color: Colors.white,
+        child: Stack(
+          children: [
+            Column(
+              children: [
+                GestureDetector(
+                  onTap: () {
+
+                  },
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10.0),
+                        child: Container(
+                          width: 150,
+                          height: 220,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage(
+                                  'assets/images/vector_smart_object_1.png'),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: 110,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage(
+                                'assets/images/layer_7_copy_2.png'),
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 30.0,
+                ),
+                _menuOptions('assets/images/l_0001nav.png', 'خصوصیات'),
+                SizedBox(
+                  height: 10.0,
+                ),
+                _menuOptions('assets/images/group_48.png', 'کچھ ہمارے بارے میں'),
+                SizedBox(
+                  height: 10.0,
+                ),
+                _menuOptions('assets/images/forma_1nav.png', 'تعاون'),
+                SizedBox(
+                  height: 10.0,
+                ),
+                _menuOptions('assets/images/l_1.png', 'رابطہ'),
+                SizedBox(
+                  height: 30.0,
+                ),
+              ],
+            ),
+
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Container(
+                width: 150,
+                height: 130,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(
+                        'assets/images/pattern.png'),
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),)
+          ],
+
+        ),
+      ),
+    );
+  }
+
+  Widget _menuOptions(String assetimage, String menuOptionIs) {
+    return OpenContainer(
+      transitionType: ContainerTransitionType.fadeThrough,
+      transitionDuration: Duration(
+        milliseconds: 500,
+      ),
+      closedElevation: 0.0,
+      openElevation: 3.0,
+      closedColor: Colors.transparent,
+      openColor: Colors.transparent,
+      middleColor: Colors.transparent,
+      onClosed: (value) {
+        // print('Profile Page Closed');
+        // if (mounted) {
+        //   setState(() {
+        //     ImportantThings.findImageUrlAndUserName();
+        //   });
+        // }
+      },
+      openBuilder: (context, openWidget) {
+        if (menuOptionIs == 'خصوصیات')
+          return PropertiesScreen();
+        else if (menuOptionIs == 'کچھ ہمارے بارے میں')
+          return AboutScreen();
+        else if (menuOptionIs == 'تعاون')
+          return SupportScreen();
+        else if (menuOptionIs == 'رابطہ') return ContactScreen();
+        return Center();
+      },
+      closedBuilder: (context, closeWidget) {
+        return SizedBox(
+          height: 40.0,
+          child: Padding(
+            padding: const EdgeInsets.only(right: 20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  menuOptionIs,
+                  style: TextStyle(
+                      fontSize: 10.0,
+                      color: Colors.grey[700],
+                      fontFamily: 'NotoNastaliqUrdu'
+                  ),
+                ),
+                SizedBox(
+                  width: 30.0,
+                ),
+                Container(
+                  width: 15,
+                  height: 15,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(assetimage),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+
+              ],
+            ),
+          ),
+        );
+      },
+    );
   }
 }
--- lib/FrontEnd/UpdateScreen.dart
+++ lib/FrontEnd/UpdateScreen.dart
@@ -1,4 +1,10 @@
+import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
+import 'package:mohaddis/FrontEnd/HomeScreen.dart';
+import 'package:mohaddis/NavMenuScreens/AboutScreen.dart';
+import 'package:mohaddis/NavMenuScreens/ContactScreen.dart';
+import 'package:mohaddis/NavMenuScreens/PropertiesScreen.dart';
+import 'package:mohaddis/NavMenuScreens/SupportScreen.dart';
 
 class UpdateScreen extends StatefulWidget {
   const UpdateScreen({Key? key}) : super(key: key);
@@ -8,8 +14,288 @@ class UpdateScreen extends StatefulWidget {
 }
 
 class _UpdateScreenState extends State<UpdateScreen> {
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      key: _scaffoldKey,
+      drawer: _drawer(),
+      backgroundColor: Colors.white,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(60), // Set this height
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(
+                  'assets/images/rectangle_798.png'),
+              fit: BoxFit.fill,
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.only(top: 30.0),
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 20.0,top: 10.0),
+                  child: Column(
+                    children: [
+                      GestureDetector(
+                        onTap:(){
+                          _scaffoldKey.currentState?.openDrawer();
+                        },
+                        child: Container(
+                          width: 35,
+                          height: 35,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage(
+                                  'assets/images/group_36.png'),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    children: [
+                      Center(
+                          child: Padding(
+                            padding: const EdgeInsets.only(top: 6.0),
+                            child: Text('اپڈیٹس ',
+                              style: TextStyle(
+                                  fontSize: 14.0,
+                                  color: Colors.white,
+                                  fontFamily: 'NotoNastaliqUrdu'
+                              ),),
+                          )
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 20.0,top: 20.0),
+                  child: Column(
+                    children: [
+                      GestureDetector(
+                        onTap:(){
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (_) => HomeScreen()));
+                        },
+                        child: Container(
+                          width: 25,
+                          height: 15,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage(
+                                  'assets/images/group_38.png'),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          SizedBox(height: 55.0,),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Container(
+                child: Text('تمام اپڈیٹس ',
+                  style: TextStyle(
+                      fontSize: 20.0,
+                      fontFamily: 'NotoNastaliqUrdu'
+                  ),)
+            ),
+          ),
+          SizedBox(height: 50,),
+          Container(
+            color: Colors.grey,
+          ),
+          Expanded(
+            child: Align(
+              alignment: FractionalOffset.bottomCenter,
+              child: Container(
+                width: double.maxFinite,
+                height: 130,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(
+                        'assets/images/layer_1699.png'),
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+
+
+  Widget _drawer(){
+    return Drawer(
+      elevation: 10.0,
+      child: Container(
+        width: double.maxFinite,
+        height: double.maxFinite,
+        color: Colors.white,
+        child: Stack(
+          children: [
+            Column(
+              children: [
+                GestureDetector(
+                  onTap: () {
+
+                  },
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10.0),
+                        child: Container(
+                          width: 150,
+                          height: 220,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage(
+                                  'assets/images/vector_smart_object_1.png'),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: 110,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage(
+                                'assets/images/layer_7_copy_2.png'),
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 30.0,
+                ),
+                _menuOptions('assets/images/l_0001nav.png', 'خصوصیات'),
+                SizedBox(
+                  height: 10.0,
+                ),
+                _menuOptions('assets/images/group_48.png', 'کچھ ہمارے بارے میں'),
+                SizedBox(
+                  height: 10.0,
+                ),
+                _menuOptions('assets/images/forma_1nav.png', 'تعاون'),
+                SizedBox(
+                  height: 10.0,
+                ),
+                _menuOptions('assets/images/l_1.png', 'رابطہ'),
+                SizedBox(
+                  height: 30.0,
+                ),
+              ],
+            ),
+
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Container(
+                width: 150,
+                height: 130,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(
+                        'assets/images/pattern.png'),
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),)
+          ],
+
+        ),
+      ),
+    );
+  }
+
+  Widget _menuOptions(String assetimage, String menuOptionIs) {
+    return OpenContainer(
+      transitionType: ContainerTransitionType.fadeThrough,
+      transitionDuration: Duration(
+        milliseconds: 500,
+      ),
+      closedElevation: 0.0,
+      openElevation: 3.0,
+      closedColor: Colors.transparent,
+      openColor: Colors.transparent,
+      middleColor: Colors.transparent,
+      onClosed: (value) {
+        // print('Profile Page Closed');
+        // if (mounted) {
+        //   setState(() {
+        //     ImportantThings.findImageUrlAndUserName();
+        //   });
+        // }
+      },
+      openBuilder: (context, openWidget) {
+        if (menuOptionIs == 'خصوصیات')
+          return PropertiesScreen();
+        else if (menuOptionIs == 'کچھ ہمارے بارے میں')
+          return AboutScreen();
+        else if (menuOptionIs == 'تعاون')
+          return SupportScreen();
+        else if (menuOptionIs == 'رابطہ') return ContactScreen();
+        return Center();
+      },
+      closedBuilder: (context, closeWidget) {
+        return SizedBox(
+          height: 40.0,
+          child: Padding(
+            padding: const EdgeInsets.only(right: 20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  menuOptionIs,
+                  style: TextStyle(
+                      fontSize: 10.0,
+                      color: Colors.grey[700],
+                      fontFamily: 'NotoNastaliqUrdu'
+                  ),
+                ),
+                SizedBox(
+                  width: 30.0,
+                ),
+                Container(
+                  width: 15,
+                  height: 15,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(assetimage),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+
+              ],
+            ),
+          ),
+        );
+      },
+    );
   }
 }
